afcabf75a431e3b1785065a345ea4eeba6a22109
kec <57247368+yesky85@users.noreply.github.com>
2020-05-26 14:26:50 +0900

    Initial db

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () =>
   console.log(`Listening at http://localhost:${PORT}`);
