62b1e12a0384b8d010a5617e961e2a29cb1b35a5
kec <57247368+yesky85@users.noreply.github.com>
2020-05-26 23:12:13 +0900

    Initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+module.exports = {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+};
