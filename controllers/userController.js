80097cc237a877e3583c0c95f8d2166c5a9f9b33
kec <57247368+yesky85@users.noreply.github.com>
2020-05-27 15:26:40 +0900

    Initial scss

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,7 @@ export const postLogin = (req, res) => {
 export const logout = (req, res) => res.redirect(routes.home);
 
 export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail", video: [] });
 
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
